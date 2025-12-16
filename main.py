e414ca94358b4504dabda3fa213a624926ff7026
Graziela Vaz <grazimvaz@gmail.com>
2025-12-16 19:29:50 -0300

    teste telegram isolado

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import asyncio
-import threading
 from telegram_bot import iniciar_bot
-from trader import iniciar_trader
 
-print("🔥 MAIN.PY CARREGADO COM SUCESSO")
+print("🔥 TESTE TELEGRAM PURO")
 
-# Trader em thread
-threading.Thread(
-    target=iniciar_trader,
-    daemon=True
-).start()
-
-print("📈 Trader iniciado (modo contínuo)")
-
-# Telegram é o processo principal
 asyncio.run(iniciar_bot())
